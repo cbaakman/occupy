290bdab20e4aa4c90a2b2ac03405c576b3dae90f
cbaakman <cbaakman@gmail.com>
2018-07-10 22:15:13 +0200

    update needs to include object class to let the reciever duplicate it

--- src/main/java/net/cbaakman/occupy/network/Update.java
+++ src/main/java/net/cbaakman/occupy/network/Update.java
@@ -8,12 +8,14 @@ import lombok.Data;
 @Data
 public class Update implements Serializable {
 	
-	public Update(UUID objectID, String fieldID, Object value) {
+	public Update(Class<? extends Updatable> objectClass, UUID objectID, String fieldID, Object value) {
+		this.objectClass = objectClass;
 		this.objectID = objectID;
 		this.fieldID = fieldID;
 		this.value = value;
 	}
 	
+	private Class<? extends Updatable> objectClass;
 	private UUID objectID;
 	private String fieldID;
 	private Object value;
