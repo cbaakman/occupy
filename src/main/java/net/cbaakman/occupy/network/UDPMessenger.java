7ab75ae38976e897a6be1350cf48dcd45adb5f3a
cbaakman <cbaakman@gmail.com>
2018-07-11 23:08:02 +0200

    replace messenger by udpmessenger

--- src/main/java/net/cbaakman/occupy/network/UDPMessenger.java
+++ src/main/java/net/cbaakman/occupy/network/UDPMessenger.java
@@ -1,21 +1,23 @@
 package net.cbaakman.occupy.network;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketAddress;
 import java.net.SocketException;
 import java.util.UUID;
 
 import javax.annotation.PreDestroy;
 
-public abstract class UDPMessenger extends Messenger {
+public abstract class UDPMessenger {
 
 	private DatagramSocket socket;
 	private Thread listenerThread;
-	
 	
 	public UDPMessenger(int listenPort) throws SocketException {
 		socket = new DatagramSocket(listenPort);
@@ -23,19 +25,27 @@ public abstract class UDPMessenger extends Messenger {
 		listenerThread = new Thread() {
 			public void run() {
 				
+				byte[] data = new byte[1024];
+				DatagramPacket packet = new DatagramPacket(data, data.length);
+				
+				ByteArrayInputStream bais;
+				ObjectInputStream ois;
+				Message message;
+				
 				while (!Thread.currentThread().isInterrupted()) {
-					
-					DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
-					
-					synchronized(socket) {
-						try {
+					try {
+						synchronized(socket) {
 							socket.receive(packet);
-							
-							
-							
-						} catch (IOException e) {
-							e.printStackTrace();
 						}
+						
+						bais = new ByteArrayInputStream(packet.getData());
+						ois = new ObjectInputStream(bais);
+						
+						message = (Message)ois.readObject();
+						onReceive(packet.getAddress(), packet.getPort(), message);
+						
+					} catch (IOException | ClassNotFoundException e) {
+						e.printStackTrace();
 					}
 				}
 			}
@@ -43,27 +53,30 @@ public abstract class UDPMessenger extends Messenger {
 		listenerThread.start();
 	}
 	
-	@Override
-	void send(UUID receiverId, Message message) {
-		
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    ObjectOutputStream oos = new ObjectOutputStream(baos);
-	    oos.writeObject(getId());
-	    oos.writeObject(message);
-	    oos.close();
-	    
-	    Contact contact = getContact(receiverId);
+	abstract void onReceive(InetAddress senderAddress, int senderPort, Message message);
+	
+	void send(InetAddress receiverAddress, int receiverPort, Message message) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    ObjectOutputStream oos = new ObjectOutputStream(baos);
+		    oos.writeObject(message);
+		    oos.close();
 
-		DatagramPacket packet = new DatagramPacket(baos.toByteArray(), baos.size(), contact.getAddress(), contact.getPort());
+		    DatagramPacket packet = new DatagramPacket(baos.toByteArray(), baos.size(), receiverAddress, receiverPort);
 
-		synchronized(socket) {
-			socket.send(packet);
+			synchronized(socket) {
+				socket.send(packet);
+			}	
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 	@PreDestroy
 	void disconnect() {
-		socket.disconnect();
+		synchronized(socket) {
+			socket.disconnect();
+		}
 		listenerThread.interrupt();
 	}
 }
