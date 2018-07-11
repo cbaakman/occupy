da1101d016c3407a826de8cdd3bb62ce255b756c
cbaakman <cbaakman@gmail.com>
2018-07-11 15:53:08 +0200

    helper functions

--- src/main/java/net/cbaakman/occupy/network/Updater.java
+++ src/main/java/net/cbaakman/occupy/network/Updater.java
@@ -1,12 +1,19 @@
 package net.cbaakman.occupy.network;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import net.cbaakman.occupy.network.enums.MessageType;
 import lombok.AccessLevel;
 
 @Data
@@ -24,4 +31,65 @@ public abstract class Updater {
 	protected Map<UUID, Updatable> updatables = new HashMap<UUID, Updatable>();
 	
 	public abstract void update(final float dt);
+	
+	protected List<Update> getUpdatesWith(Class<? extends Annotation> annotationClass) {
+		
+		List<Update> updates = new ArrayList<Update>();
+		
+		for (Entry<UUID, Updatable> entry : updatables.entrySet()) {
+			UUID objectId = entry.getKey();
+			Updatable updatable = entry.getValue();
+			
+			Class<? extends Updatable> objectClass = updatable.getClass();
+			
+			for (Field field : objectClass.getDeclaredFields()) {
+				
+				if (field.isAnnotationPresent(annotationClass)) {
+					
+					field.setAccessible(true);
+			
+					try {
+						Update update = new Update(objectClass, objectId, field.getName(), field.get(updatable));
+						
+						updates.add(update);
+						
+					} catch (IllegalArgumentException | IllegalAccessException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+		return updates;
+	}
+	
+	protected void processUpdateWith(Update update, Class<? extends Annotation> annotationClass) {
+		
+		Updatable updatable = updatables.get(update.getObjectID());
+		if (updatable == null) {
+			try {
+				updatable = update.getObjectClass().newInstance();
+				
+				updatables.put(update.getObjectID(), updatable);
+				
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+		
+		try {
+			Field field = update.getObjectClass().getField(update.getFieldID());
+			
+			if (field.isAnnotationPresent(annotationClass)) {
+			
+				field.setAccessible(true);
+			
+				field.set(updatable, update.getValue());
+			}
+			
+		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
 }
