67ec31d6653afe3ec1d391b12e06ff626172f427
cbaakman <cbaakman@gmail.com>
2018-07-23 21:34:38 +0200

    make isReady optional

--- src/main/java/net/cbaakman/occupy/load/LoadJob.java
+++ src/main/java/net/cbaakman/occupy/load/LoadJob.java
@@ -2,11 +2,13 @@ package net.cbaakman.occupy.load;
 
 import java.util.concurrent.Callable;
 
-public interface LoadJob<T> extends Callable<T> {
+public abstract class LoadJob<T> implements Callable<T> {
 	
 	/**
 	 * Should only return true if the job is ready to run.
 	 * That is: all other jobs that this one depends on have finished.
 	 */
-	public boolean isReady();
+	public boolean isReady() {
+		return true;
+	}
 }
